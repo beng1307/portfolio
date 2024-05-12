c8982983f06e661db1505c4a453c71bc0cd43bd6
Ben <benjamin.gretic@gmail.com>
2024-05-12 23:19:07 +0200

    corrected a typo in lstnew

--- Libft/ft_lstnew.c
+++ Libft/ft_lstnew.c
@@ -4,7 +4,7 @@ t_list *ft_lstnew(void *content)
 {
     t_list *new_node;
 
-    new_node = malloc(size_of(t_list));
+    new_node = malloc(sizeof(t_list));
     if (!new_node)
         return (NULL);
     new_node->content = content;
