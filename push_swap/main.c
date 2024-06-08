b5f81bd5af01488a0ac3a358585015417eea796e
Ben <benjamin.gretic@gmail.com>
2024-06-09 01:19:16 +0200

    New things added

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,9 @@ int main(int ac, char **av)
     t_list *stack_b;
 
     if (ac < 2)
-        return (ft_putendl_fd("Error", 2), -1);
-    
-    return (0);
+        return (ft_putendl_fd ("Error", 2), -1);
+	sort_it(stack_a, stack_b);
+	
+
+	return (0);
 }
