29d17d9f625137fb60bae94d51a79a7cfb645d29
Benjamin Gretic <benson@pc.home>
2024-07-15 13:06:53 +0200

    [ush

--- push_swap/my_sort.c
+++ push_swap/my_sort.c
@@ -12,35 +12,34 @@ static int	get_middle_value(t_list *stack, size_t stack_size)
 		return_value += counting_node->content;
 		counting_node = counting_node->next;
 	}
-	return (return_value / stack_size);
++	return (return_value / stack_size);
 }
 
 static void	split_stack(t_list **stack_a, t_list **stack_b)
 {
 	t_list	*node;
-	size_t	stack_size;
 	int		middle_value;
+	int		stack_size;
 	static int count = 0;
 
 	node = *stack_a;
 	stack_size = ft_lstsize(*stack_a);
 	middle_value = get_middle_value(*stack_a, stack_size);
-	while (stack_size && node)
+	while (stack_size > 0)
 	{
 		if (node->content <= middle_value)
 		{
-			node = node->next;
 			ft_pb(stack_a, stack_b);
 			ft_putnbr_fd(count++, 1);
 			ft_putchar_fd('\n', 1);
 		}
-		else
+		else if (node->content > middle_value)
 		{
-			node = node->next;
 			ft_ra(stack_a);
 			ft_putnbr_fd(count++, 1);
 			ft_putchar_fd('\n', 1);
 		}
+		node = *stack_a;
 		stack_size--;
 	}
 }
@@ -49,7 +48,7 @@ void	sort_it(t_list **stack_a, t_list **stack_b, int stack_size)
 {
 	if (stack_size <= 1)
 		return ;
-	split_stack(stack_a, stack_b);
+	split_stack(stack_b, stack_a);
 	sort_it(stack_a, stack_b, ft_lstsize(*stack_a));
 	sort_it(stack_b, stack_a, ft_lstsize(*stack_b));
 	while (*stack_b)
